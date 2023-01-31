cee48ddfbcaa03d7cb976559b33a3d9ec540919e
Nico404 <nicolas.geron@gmail.com>
2023-01-31 01:36:05 +0100

    cleanup deprecated functions

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
-import time
-from datetime import datetime, timedelta
-from tqdm import tqdm
+from tqdm import *
 
-from src.url_generator import posts_url_generator, comments_url_generator
-from src.get_submission import get_submission
-from src.parse_submission import parse_posts, parse_comments
 from src.save_csv import save_csv
-from src.utc_day import utc_day
+from src.get_submission_API import get_posts, get_comments
+from src.parse_submission_API import parse_posts, parse_comments
+from private.get_token import get_token
 
 
-start_datetime = datetime(2023, 1, 27, 0, 0)
-end_datetime = datetime(2017, 1, 1, 0, 0)
+access_token = get_token()
+subreddit = "AmItheAsshole"
 
-day_list = utc_day(start_datetime, end_datetime)
+submission_posts = get_posts(access_token, subreddit)  # first call when after is None
+posts, after = parse_posts(submission_posts)
 
-for day in (progress_bar := tqdm(day_list)):
-    progress_bar.set_description(f"processing {day}")
-
-    url = posts_url_generator(day[1], day[2])
-    try:
-        posts = get_submission(url)
-    except:
-        print("error getting submission post")
-    save_csv(parse_posts(posts), "posts")  # writes content of API URL to csv file
-
+for loop in tqdm(range(50000)):
+    posts, after = parse_posts(get_posts(access_token, subreddit, after=after))
+    save_csv(posts, "posts_v2_2")
     for post in posts:
-        url = comments_url_generator(post["id"])
-        try:
-            comments = get_submission(url)
-        except:
-            print("error getting submission comment")
-        save_csv(parse_comments(comments), "comments")
+        submission_comments = get_comments(access_token, subreddit, post[0])
+        comments = parse_comments(submission_comments)
+        save_csv(comments, "comments_v2_2")
--- src/save_csv.py
+++ src/save_csv.py
@@ -2,15 +2,15 @@ import csv
 
 
 def save_csv(tuple_list, file_name):
-    """writes a csv line for each submission
+    """writes to csv for each submission block
 
     Args:
-        tuple_list (list): takes a list of tuples from parse_posts or parse_comments
-        with submission info
+        tuple_list (list): takes a list of tuples from a parsed posts
+        or comment with submission info
+        file_name (string): name of file we want to write to
     """
     with open("data/" + file_name + ".csv", "a") as file:  # append mode
         csv_content = csv.writer(file)
-        # csv_content.writerow(['title', 'num_comments', 'created_utc', 'url', 'id', 'selftext'])
         csv_content.writerows(tuple_list)
-        print(len(tuple_list), "submissions written to " + file_name + " csv file.")
-    return len(tuple_list)
+        # print(len(tuple_list), "submissions written to " + file_name + " csv file.")
+    # return len(tuple_list)
